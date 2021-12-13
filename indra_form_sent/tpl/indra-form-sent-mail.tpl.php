02e615606ae5fd557d1c9fc9d809610c5995c6a9
INDRA\mrivas <mrivas@minsait.com>
2021-12-13 11:52:11 +0100

    testing

--- indra_form_sent/tpl/indra-form-sent-mail.tpl.php
+++
@@ -1,7 +0,0 @@
-<div class="form-sent-message">
-    <div><strong><?php print t('Subject') . ': '; ?></strong><?php print $subject; ?></div>
-    <div><strong><?php print t('To') . ': '; ?></strong><?php print $mailto; ?></div>
-    <div><strong><?php print t('Reply') . ': '; ?></strong><?php print $replyto; ?></div>
-    <div><strong><?php print t('Body') . ': '; ?></strong></div>
-    <div><?php print $body; ?></div>
-</div>
